bd4114f60235ce63126ffbed53114e7b608eae0e
Liv Vitale <liv.isabella.vitale@gmail.com>
2019-03-21 16:32:04 -0700

    User input

--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -1,6 +1,10 @@
-import styled from "styled-components";
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight } from '@fortawesome/pro-light-svg-icons';
+import { Color } from '../theme';
 
-const UserInput = styled.input`
+const Input = styled.input`
   height: 42px;
   border-radius: 6px;
   border: 1px solid #ddd;
@@ -10,5 +14,44 @@ const UserInput = styled.input`
   padding: 0 12px;
   margin-right: 12px;
 `;
+
+const UserInputWrapper = styled.div`
+  display: flex;
+  position: sticky;
+  bottom: 0;
+  padding: 12px;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+`;
+
+const UserInput = ({ onMessageEntered }) => {
+  const [value, setValue] = useState('');
+
+  const handleChange = e => {
+    setValue(e.target.value);
+  };
+
+  const handleKeyDown = e => {
+    if (e.key === 'Enter') {
+      // Fire callback to handle message
+      onMessageEntered(value);
+
+      // Clear input
+      setValue('');
+    }
+  };
+
+  return (
+    <UserInputWrapper>
+      <Input value={value} onChange={handleChange} onKeyDown={handleKeyDown} />
+      <FontAwesomeIcon
+        icon={faArrowRight}
+        size="2x"
+        color={Color['orange-400']}
+      />
+    </UserInputWrapper>
+  );
+};
 
 export default UserInput;
