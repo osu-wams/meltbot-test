1c9e0ad7621a76059c4a0d338de5558cda33ad8c
Mauricio Cordoba <mauricio.cordoba@oregonstate.edu>
2019-03-27 16:11:38 -0700

    Message componenet snapshot tests for all 3 types

---
+++ src/components/__tests__/Message.test.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { render } from 'react-testing-library';
+import Message from '../Message';
+
+const BotMessage = () => (
+  <Message type="bot">
+    <p>Bot Message</p>
+  </Message>
+);
+
+const UserMessage = () => (
+  <Message type="user">
+    <p>User Message</p>
+  </Message>
+);
+
+const FollowUpQuestion = () => (
+  <Message>
+    <p>Follow up question button</p>
+  </Message>
+);
+
+test('BotMessage - Message with type="bot" is styled appropriately', () => {
+  const { container } = render(<BotMessage />);
+  expect(container).toMatchSnapshot();
+});
+
+test('UserMessage - Message with type="user" is styled appropriately', () => {
+  const { container } = render(<UserMessage />);
+  expect(container).toMatchSnapshot();
+});
+
+test('FollowUpQuestion - Message with no type - is styled appropriately', () => {
+  const { container } = render(<FollowUpQuestion />);
+  expect(container).toMatchSnapshot();
+});
