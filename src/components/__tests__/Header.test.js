35f6d1b06d43b6425355dc1fafc897b4607c24b3
Mauricio Cordoba <mauricio.cordoba@oregonstate.edu>
2019-03-28 15:10:01 -0700

    Cleanup and small modifications to input and dialog

--- src/components/__tests__/Header.test.js
+++ src/components/__tests__/Header.test.js
@@ -14,13 +14,19 @@ test('Renders without errors', () => {
 
 test('Modal will open and close when appropriate buttons are clicked', async () => {
   const { getByText, getByTestId } = render(<Header />);
+
+  // Click on question icon
   const openBtn = getByTestId('about-benny');
   fireEvent.click(openBtn);
+
+  // Verify Modal is present
   const aboutModal = await waitForElement(() => getByText('Hi, I’m Benny!'));
   expect(aboutModal).toBeInTheDocument();
 
+  // Click close button on modal
   const closeBtn = getByTestId('close-about');
   fireEvent.click(closeBtn);
 
+  // Verify modal closed
   expect(aboutModal).not.toBeInTheDocument();
 });
